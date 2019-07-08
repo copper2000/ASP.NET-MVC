9ad4d3850148a7ede48366d487dc960626aa665a
nhatnguyen741@gmail.com <khaibaohuan2000>
2019-07-08 17:44:04 +0700

    add code 001

---
+++ OnlineShop/Model/Dao/UserDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext db = null;
+        public UserDao() 
+        {
+            db = new OnlineShopDbContext();
+        }
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public User GetById(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+        public bool Login(String userName, String passWord) 
+        {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == passWord);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Nhập lại tên đăng nhập")]
+        public String UserName { set; get; }
+        [Required(ErrorMessage = "Nhập lại mật khẩu")]
+        public String Password { set; get; }
+        public Boolean Remember { set; get; }          
+    }
+}
