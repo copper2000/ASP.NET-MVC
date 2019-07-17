052cb2bf525925ad452229d912ae4f842c85df3b
nhatnguyen741@gmail.com <khaibaohuan2000>
2019-07-17 17:43:49 +0700

    add code 013

---
+++ OnlineShop/OnlineShop/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Slides = new SlideDao().ListAll();
+            var productDao = new ProductDao();
+            ViewBag.NewProduct = productDao.ListNewProduct(4);
+            ViewBag.FeatureProduct = productDao.ListFeatureProduct(4);
+            return View();            
+        }
+        [ChildActionOnly]
+        public ActionResult MainMenu()
+        {
+            var model = new MenuDao().ListGroupById(1);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        public ActionResult TopMenu()
+        {
+            var model = new MenuDao().ListGroupById(2);
+            return PartialView(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().GetFooter();
+            return PartialView(model);
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dao;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult ProductCategory()
+        {
+            var model = new ProductCategoryDao().ListAll();
+            return PartialView(model);
+        }
+    }
+}
