025562d1573a73f35242cfc3a0c1ca63b2b0ede4
nhatnguyen741@gmail.com <khaibaohuan2000>
2019-07-18 17:30:37 +0700

    code need fix 001

--- OnlineShop/Model/Dao/ProductDao.cs
+++ OnlineShop/Model/Dao/ProductDao.cs
@@ -15,12 +15,27 @@ namespace Model.Dao
         {
             db = new OnlineShopDbContext();
         }
+        /// <summary>
+        /// GET LIST PRODUCT BY CATEGORY
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public List<Product> ListByCategoryId(long categoryID, ref int totalRecord, int pageIndex = 1, int pageSize = 2)
+        {
+            totalRecord = db.Products.Where(x => x.CategoryID == categoryID).Count();
+            var model = db.Products.Where(x => x.CategoryID == categoryID).OrderByDescending(x => x.CreateDate).Skip((pageIndex-1)*pageSize).Take(pageSize).ToList();
+            return model;
+        }
 
         public List<Product> ListNewProduct(int top)
         {
             return db.Products.OrderByDescending(x => x.CreateDate).Take(top).ToList();
         }
-
+        /// <summary>
+        /// LIST FEATURE PRODUCT
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
         public List<Product> ListFeatureProduct(int top)
         {
             return db.Products.Where(x => x.TopHot != null && x.TopHot > DateTime.Now).OrderByDescending(x => x.CreateDate).ToList();
--- OnlineShop/OnlineShop/Controllers/ProductController.cs
+++ OnlineShop/OnlineShop/Controllers/ProductController.cs
@@ -23,10 +23,27 @@ namespace OnlineShop.Controllers
             var model = new ProductCategoryDao().ListAll();
             return PartialView(model);
         }
-        public ActionResult Category(long id)
+        public ActionResult Category(long id, int page = 1, int pageSize = 2)
         {
             var category = new CategoryDao().ViewDatail(id);
-            return View(category);
+            ViewBag.Category = category;
+            int totalRecord = 0;
+            var model = new ProductDao().ListByCategoryId(id, ref totalRecord, page, pageSize);
+
+            ViewBag.Total = totalRecord;
+            ViewBag.Page = page;
+
+            int maxPage = 5;
+            int totalPage = 0;
+             
+            totalPage = (int)Math.Ceiling((double)(totalRecord/pageSize));
+            ViewBag.TotalPage = totalPage;
+            ViewBag.MaxPage = maxPage;
+            ViewBag.First = 1;
+            ViewBag.Last = totalPage;
+            ViewBag.Next = page + 1;
+            ViewBag.Previous = page - 1;
+            return View(model);
         }
         public ActionResult Detail(long id)
         {
